76ebef68befe5e743d638ee77ba458a2149bac6d
Leon Jun Wei Lam <llam@olin.edu>
2017-02-01 20:38:24 -0500

    changing port back to 0.0.0.0 (was localhost previously)

--- server.py
+++ server.py
@@ -103,6 +103,6 @@ def event_submitted():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', debug=True, port=port)
-    app.run(host='127.0.0.1', debug=True, port=port)
+    app.run(host='0.0.0.0', debug=True, port=port)
+    # app.run(host='127.0.0.1', debug=True, port=port)
 
