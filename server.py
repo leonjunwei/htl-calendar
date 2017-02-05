b8a80725e63cb3d2ca008493cbe9886c2c303584
Leon Jun Wei Lam <llam@olin.edu>
2017-02-05 18:14:08 -0500

    changes to server.py, event submission

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ import pickle
 
 import urllib.parse
 
-# import pandas as pd
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
@@ -15,10 +14,57 @@ display_month = current_time.month-1
 display_year = current_time.year
 
 
-##Database version##
+## To-Do ##
+
+
+# Figure out how the columns of the database should be laid out
+"""
+1) Unique Event ID (maybe allow users to override? What if there's an event by a different user with the same password they want?)
+2) Event Name
+3) Event Taglist (string like "coding from_website limited_number"? A list of all tags ranked by frequency should be accessible somewhere.)
+4) Event start datetime (Allowing users to select date/time of start and date/time of end independently will be good)
+5) Event end datetime   (If they don't put an end date we assume it's the same as the start date)
+6) Event Location
+7) Event Summary
+8) Link/email to event (something people can click for even more details.)
+
+
+"""
+
+
+# Users may want to search by event name, event tag (submission, official, carpe might be some of the tags), location, 
+# any combination of (starts earlier than, starts exactly at, starts later than) and (ends earlier than, ends exactly at, ends later than)
+# unique event ID would be good too
+
+
+# figure out if SQL has >= or whether > works
+
+
+# python datetime objects can be stored in postgres via psycopg's cursor.execute(instruction,(data1,)). 
+# Even if there's only one piece of data it still has to be in a tuple
+
+
+
+## Useful Resources ##
+
+# https://devcenter.heroku.com/articles/heroku-postgresql
+# ^ How to get postgres working for the heroku app
+
+# http://initd.org/psycopg/docs/usage.html
+# ^ How to use psycopg2 to connect to/access/edit a postgres database.
+
+
+
+## Database access/edit ##
+
 import psycopg2
 urllib.parse.uses_netloc.append("postgres")
 url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
+
+
+def generate_random_key(): #change this to gfycat-style 3-word concatenation when it actually becomes necessary
+    from random import random
+    return random()
 
 
 def make_conn():
@@ -35,8 +81,8 @@ def interact_with_database(instruction): #this is A VERY BAD IDEA if you don't w
     store = None
     conn = make_conn()
     with conn.cursor() as cur:
-        cur.execute(instruction)
         try:
+            cur.execute(instruction)
             store = [row for row in cur]
         except:
             pass
@@ -46,6 +92,28 @@ def interact_with_database(instruction): #this is A VERY BAD IDEA if you don't w
         return "Your instruction was " + str(instruction) + " . Cursor output (if any) is: " + str(store)
     else:
         return "Your instruction was " + str(instruction)
+
+def add_event_into_database(data): 
+    """
+    data must be a tuple, and its format must follow the table's format.
+    data[0] should be the data that needs to be in the leftmost column of table, data[1] should be the next one and so on.
+    """
+    flag = None
+    conn = make_conn()
+    with conn.cursor as cur:
+        try:
+            cur.execute("insert into events VALUES %s", data)
+            flag = 1
+        except:
+            pass
+    conn.commit()
+    conn.close()
+    return flag #flag tells us if the query succeeded.
+
+def add_form_to_database(form):
+    pass
+
+## Site Map ##
 
 @app.route('/',methods = ["GET","POST"])
 @app.route('/index.html',methods = ["GET","POST"])
@@ -84,121 +152,52 @@ def test():
             return render_template('test.html', data = message)
 
 
+@app.route('/',methods = ["GET","POST"])
+@app.route('/index.html',methods = ["GET","POST"])
+def home_page():
+    global display_month
+    global display_year
+    if request.method == "GET":
+        display_month = current_time.month-1
+        display_year = current_time.year
+        return render_template('index.html', month=current_time.month-1, year = current_time.year)
+    else:
+        if request.form['direction']=='forward':
+            display_month += 1
+            if display_month > 11:
+                display_year += 1
+                display_month = 0
+        elif request.form['direction']=='backward':
+            display_month -= 1
+            if display_month < 0:
+                display_year -= 1
+                display_month = 11
+        return render_template('index.html', month=display_month, year=display_year)
 
 
+@app.route('/event_submission.html',methods = ["GET","POST"]) #page where users can submit events
+def event_submission():
+    if request.method == "GET":
+        return render_template('event_submission.html')
+    else:
+        return render_template('event_submitted.html', data = interact_with_database("select * from events"))
 
 
-## Local Calendar Version ##
-
-
-# with open('calendar','wb') as file:
-#     pickle.dump({},file)
-
-
-# with open('calendar','rb') as file:
-#     store = pickle.load(file)
-
-# print(store)
-
-
-# @app.route('/health')
-# def health():
-#     return 'ok'
-
-
-# @app.route('/',methods = ["GET","POST"])
-# @app.route('/index.html',methods = ["GET","POST"])
-# def home_page():
-#     global display_month
-#     global display_year
-#     if request.method == "GET":
-#         display_month = current_time.month-1
-#         display_year = current_time.year
-#         return render_template('index.html', month=current_time.month-1, year = current_time.year)
-#     else:
-#         if request.form['direction']=='forward':
-#             display_month += 1
-#             if display_month > 11:
-#                 display_year += 1
-#                 display_month = 0
-#         elif request.form['direction']=='backward':
-#             display_month -= 1
-#             if display_month < 0:
-#                 display_year -= 1
-#                 display_month = 11
-#         return render_template('index.html', month=display_month, year=display_year)
-
-
-# def add_to_dictionary(form):
-#     import random
-#     with open('calendar','rb') as file:
-#         store = pickle.load(file)
-#         uniqueID = random.random() #between 0 and 1
-#         while uniqueID in store:
-#             uniqueID = random.random()
-#         store[uniqueID] = form #store contains randomKey:eventDictionary pairs, eventDictionary is another dictionary with event details
-#     with open('calendar','wb') as file1:
-#         pickle.dump(store,file1)
-#     return uniqueID
-
-# def read_from_dictionary():
-#     with open('calendar','rb') as file:
-#         store = pickle.load(file)
-#     return str(store)
-
-# def wipe_everything():
-#     with open('calendar','wb') as file:
-#         pickle.dump({},file)
-
-
-
-# @app.route('/event_page.html', methods = ["GET","POST"])
-# def event_page():
-#     if request.method == "GET":
-#         print("poo")
-#         return render_template('event_page.html')
-#     if request.method == "POST":
-#         print("event submitted")
-#         print("Your unique ID for this event is: " + str(add_to_dictionary(request.form)))
-#         return render_template('event_page.html')
-
-
-# @app.route('/event_submission.html',methods = ["GET","POST"]) #page where users can submit events
-# def event_submission():
-#     if request.method == "GET":
-#         return render_template('event_submission.html')
-#     else:
-#         return render_template('event_submitted.html', data = read_from_dictionary())
-
-
-# @app.route('/event_submitted.html',methods = ["GET","POST"]) #page afterward that shows submission status
-# def event_submitted():
-#     if request.method == "POST":
-#         try:
-#             message = "Your event has been successfully submitted! Your unique ID for this event is: " + str(add_to_dictionary(request.form))        
-#         except:
-#             message = "Something went wrong - sorry!"
-#         return render_template('event_submitted.html', data = message)
-#     elif request.method == "GET":
-#         message = "You really shouldn't have gotten here this way."
-#         return render_template('event_submitted.html', data = message)
-
-# @app.route('/wipe_everything.html',methods = ["GET","POST"])
-# def wipeEverything():
-#     if request.method == "POST":
-#         wipe_everything()
-#         message = "Wiped everything!"
-#         return render_template('event_submitted.html', data = message)
-#     elif request.method == "GET":
-#         message = "You really shouldn't have gotten here this way."
-#         return render_template('event_submitted.html', data = message)
-
-
+@app.route('/event_submitted.html',methods = ["GET","POST"]) #page afterward that shows submission status
+def event_submitted():
+    if request.method == "POST":
+        try:
+            message = "Your event has been successfully submitted! Your unique ID for this event is: " + str(add_to_dictionary(request.form))        
+        except:
+            message = "Something went wrong - sorry!"
+        return render_template('event_submitted.html', data = message)
+    elif request.method == "GET":
+        message = "You really shouldn't have gotten here this way."
+        return render_template('event_submitted.html', data = message)
 
 
 
 ##Run##
-
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000)) #not sure this will work
