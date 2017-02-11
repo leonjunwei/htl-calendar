a7e679db6c1e62948c1858c78ce617a412ee32f2
YehEmily <emilyyeh1997@gmail.com>
2017-02-10 21:15:33 -0500

    added javascript to display events on days

---
+++ test.js
@@ -0,0 +1,23 @@
+var input = [{day: '1', name: 'John'},
+{day: '2', name: 'Doe'},
+{day: '7', name: '46'},
+{day: '1', name: 'Joseph'}];
+// var grouped = _.groupBy(person,day);
+
+var output = input.reduce(function(result, value) { 
+  result[value.day] = result[value.day] || []; 
+  result[value.day].push({name: value.name, day: value.day });
+  return result; 
+}, {});
+
+console.log(output);
+
+for (i=0; i<10; i++) {
+	if (output[i + '']) {
+		for (j=0; j<10; j++) {
+			if (output[i+''][j]) {
+				console.log(output[i + ''][j]['name']);
+			}
+		}
+	}
+}
